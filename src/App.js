0683599399c0343d926f74636f7a6b147545c04a
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-08-13 15:53:56 +0530

    React Calculator app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import * as math from 'mathjs';
 import './App.css';
+import Footer from './Footer';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [result, setresult] = useState('');
+  const handleButtonClick = (value) => {
+    setInput(input + value);
+  };
+  const handleClear = () => {
+    setInput('');
+    setresult('');
+  }
+  const handleCalculate = () => {
+    try {
+      const result = math.evaluate(input);
+      if (isNaN(result) || !isFinite(result)) {
+        setresult("Error");
+      }
+      else {
+        setresult(result.toString());
+        setInput(result.toString());
+      }
+    } catch (error) {
+      setresult("Error")
+
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='input'>
+        <input type='text' value={input} readOnly />
+      </div>
+      <div className='result'>
+        <span>{result !== '' ? result : '0'}</span>
+      </div>
+      <div className='buttons'>
+        <div className='row'>
+          <button onClick={() => handleButtonClick('7')}>7</button>
+          <button onClick={() => handleButtonClick('8')}>8</button>
+          <button onClick={() => handleButtonClick('9')}>9</button>
+          <button onClick={() => handleButtonClick('/')}>/</button>
+        </div>
+        <div className='row'>
+          <button onClick={() => handleButtonClick('4')}>4</button>
+          <button onClick={() => handleButtonClick('5')}>5</button>
+          <button onClick={() => handleButtonClick('6')}>6</button>
+          <button onClick={() => handleButtonClick('*')}>*</button>
+        </div>
+        <div className='row'>
+
+          <button onClick={() => handleButtonClick('1')}>1</button>
+          <button onClick={() => handleButtonClick('2')}>2</button>
+          <button onClick={() => handleButtonClick('3')}>3</button>
+          <button onClick={() => handleButtonClick('-')}>-</button>
+          <button onClick={() => handleButtonClick('.')}>.</button>
+        </div>
+        <div className='row'>
+          <button onClick={() => handleButtonClick('0')}>0</button>
+          <button onClick={handleClear}>CLR</button>
+          <button onClick={handleCalculate}>=</button>
+          <button onClick={() => handleButtonClick('+')}>+</button>
+        </div>
+
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            Created By <a href="https://www.linkedin.com/in/abhay-tiwari-22063023b/" target='blank'>Abhay Tiwari</a> | Powered with ReactJS and MathJS
+        </div>
+    )
+}
+
+export default Footer
